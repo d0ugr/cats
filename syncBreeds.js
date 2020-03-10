519ec4a4d7287a043623de51947a42a73b20a751
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-10 17:52:32 -0400

    Added syncBreeds test

--- syncBreeds.js
+++ syncBreeds.js
@@ -13,3 +13,6 @@ const breedDetails = function(breed) {
 // get the return value right away from the function
 const bombay = breedDetails("Bombay");
 console.log(bombay); //=> prints out the description for that breed
+
+// export the function
+module.exports = breedDetails;
